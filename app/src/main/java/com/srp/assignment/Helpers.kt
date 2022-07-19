0f02cd9cb2ba11c455958957fea688fdb487eaf6
Shubham <shubham.s@boppotechnologies.com>
2022-07-19 18:00:50 +0530

    conver time

--- app/src/main/java/com/srp/assignment/Helpers.kt
+++ app/src/main/java/com/srp/assignment/Helpers.kt
@@ -2,12 +2,22 @@ package com.srp.assignment
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import java.text.SimpleDateFormat
-import java.time.LocalDate
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 class Helpers {
 
+
+    companion object {
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun convertTime(date: String?): String {
+            val zonedDateTime = ZonedDateTime.parse(date)
+            val dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+
+            return dtf.format(zonedDateTime)
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/srp/assignment/adapter/OkHttpIssuesAdapter.kt
+++ app/src/main/java/com/srp/assignment/adapter/OkHttpIssuesAdapter.kt
@@ -1,15 +1,18 @@
 package com.srp.assignment.adapter
 
 import android.content.Context
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.srp.assignment.Helpers
 import com.srp.assignment.R
 import com.srp.assignment.model.SquareIssuesDataClassItem
 import kotlinx.android.synthetic.main.item_issues.view.*
@@ -35,6 +38,7 @@ class IssuesAdapter(
 
     override fun getItemCount() = issuesList.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val restaurant = issuesList[position]
 
@@ -47,13 +51,14 @@ class IssuesAdapter(
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        @RequiresApi(Build.VERSION_CODES.O)
         fun bind(issuesList: SquareIssuesDataClassItem) {
             itemView.tvName.text = issuesList.title
             itemView.tvNumReviews.text =
                 if (issuesList.body != null && issuesList.body?.length >= 200)
                     issuesList.body?.subSequence(0, 200) ?: "" else ""
             itemView.tvAddress.text = issuesList.user?.login
-            itemView.tvCategory.text = issuesList.updatedAt
+            itemView.tvCategory.text = Helpers.convertTime(issuesList.updatedAt)
             Glide.with(context)
                 .load(issuesList.user?.avatarUrl)
                 .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(20)))
